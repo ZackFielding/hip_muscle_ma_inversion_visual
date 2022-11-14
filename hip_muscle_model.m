92b1f3110296a15048de7bb27d37c87e1630a208
Zack Fielding <zackfielding@gmail.com>
2022-11-13 22:42:39 -0700

    complete refactor - removing & updating

--- hip_muscle_model.m
+++ hip_muscle_model.m
@@ -17,62 +17,108 @@
 muscle_c = table2cell(readtable("muscle_OI.txt")); % muscle IO
 bone_c = table2cell(readtable("bony_landmark.txt")); % landmarks
 
-% generate muscle origin reference cell array for later
-for r = 1:1:size(muscle_c,1)
-    for s = 1:1:4
-        origin_ref{r,s} = muscle_c{r,s};
+ % strings for struct automation
+mb_str{1,1} = "muscle";
+mb_str{2,1} = "bone";
+
+ % row and column lengths: row (1,1), col (1,2)
+ROW_COLUMN.muscle = size(muscle_c);
+ROW_COLUMN.bone = size(bone_c);
+
+% create hash map of muscle names & array row positions
+ % create empty hash first (cell arrays do not allow vector indexing)
+IO_MAP.muscle = containers.Map('KeyType', 'char', 'ValueType', 'int32');
+for im = 1:1:ROW_COLUMN.muscle(1,1)
+    IO_MAP.muscle(muscle_c{im,1}) = im;
+end
+
+% create hash map of landmark names & array row positions
+IO_MAP.bone = containers.Map('KeyType', 'char', 'ValueType', 'int32');
+for ib = 1:1:ROW_COLUMN.bone(1,1)
+    IO_MAP.bone(bone_c{ib,1}) = ib;
+end
+
+clearvars ib im % clear loop vars
+% convert cell arrays -> normal arrays
+for i = [1 2]
+     % allocate string to improve readability in block
+    cSTR = mb_str{i,1}; % 'muscle' or 'bone'
+     % allocate NaNs to improve array alloc performance
+    IO_STRUCT(1).(cSTR) = NaN(ROW_COLUMN.(cSTR)(1,1),... % row count
+                        ROW_COLUMN.(cSTR)(1,2)-1); % col count -1
+     % temp cell array to remove need for repetitive if-else run
+    switch i
+        case 1 
+            temp_c = muscle_c;
+        case 2
+            temp_c = bone_c;
+        otherwise
+            disp("muscle and/or bone cell array not found prior to struct indexing.");
+    end
+
+    for row = 1:1:ROW_COLUMN.(cSTR)(1,1)
+        for col = 2:1:ROW_COLUMN.(cSTR)(1,2)
+            IO_STRUCT.(cSTR)(row,col-1) = temp_c{row,col}; % note col-1 for IO_s
+        end
+    end
+    clearvars temp_c
+end
+
+ % clean up workspace
+clearvars -except IO_STRUCT IO_MAP mb_str ROW_COLUMN
+% determine femoral mechanical axis (FMA) vector
+
+% Epicondyle mid point (EPI_MID)
+ % increment bone array row count -> factor for new index
+ROW_COLUMN.bone(1,1) = ROW_COLUMN.bone(1,1) + 1;
+ % index new string identifier into map
+IO_MAP.bone("EPI_MID") = ROW_COLUMN.bone(1,1);
+ % find mid way point
+IO_STRUCT(1).bone(ROW_COLUMN.bone(1,1), 1:3) = ...
+  ( (IO_STRUCT(1).bone(cell2mat(values(IO_MAP.bone, {'LFE'})), 1:3) - ...
+    IO_STRUCT(1).bone(cell2mat(values(IO_MAP.bone, {'MFE'})), 1:3)) ...
+    .* 0.5...
+   ); % = (LFE - MFE) * 0.5
+
+% Femoral mechanical axis (FMA)
+ % increment bone array row count -> factor for new index
+ROW_COLUMN.bone(1,1) = ROW_COLUMN.bone(1,1) + 1;
+ % index new string identifier into map
+IO_MAP.bone("FMA") = ROW_COLUMN.bone(1,1);
+ % find mid way point
+IO_STRUCT(1).bone(ROW_COLUMN.bone(1,1), 1:3) = ...
+  ( IO_STRUCT(1).bone(cell2mat(values(IO_MAP.bone, {'MFE'})), 1:3) + ...
+    IO_STRUCT(1).bone(cell2mat(values(IO_MAP.bone, {'EPI_MID'})), 1:3) ...
+   ); % = MFE + EPI_MID
+
+ % allocate arrays
+n_EPI_MID_ins.muscle = NaN(ROW_COLUMN.muscle(1,1), 3); % only need insertion
+n_EPI_MID_ins.bone = NaN(ROW_COLUMN.bone(1,1), 3); % original == x3 col.
+
+ % temp FMA to improve computation readability & performance
+ % most of bone result -> not used -> stays consistent with Map
+temp_FMA = IO_STRUCT(1).bone(cell2mat(values(IO_MAP.bone, {'FMA'})), 1:3);
+for i = [1 2]
+    switch i % insertion vec for for muscle - all col for bone
+        case 1
+            row_vec = 4:1:6;
+        case 2
+            row_vec = 1:1:3;
+    end
+
+    cSTR = mb_str{i,1};
+    for j = 1:1:ROW_COLUMN.(cSTR)(1,1) % need to correct for bone+2
+        n_EPI_MID_ins.(cSTR)(j, 1:3) = IO_STRUCT(1).(cSTR)(j, row_vec) - ...
+            temp_FMA;
     end
 end
+clearvars cSTR i j temp_FMA
 
- % fill bone_c with empty cells to allow cell array concatenation
-for i = 5:1:7
-    for j = 1:1:size(bone_c,1)
-        bone_c{j,i} = [];
-    end
-end
-
-mb_c = [muscle_c ; bone_c]; % concatenating cell arrays
-muscle_count = size(muscle_c,1); % get muscle count for future automation
-
-% determine femoral mechanical axis
- % assign cell array length for automated appending
-ind = size(mb_c,1)+1; % track new additions to bone_c cell array
-
- % 1/2 vec between femoral epicondyles
-mb_c{ind,1} = "Epi_Midpoint";
-[mb_c{ind, 2}, mb_c{ind, 3}, mb_c{ind, 4}] = ...
-    findVector(mb_c, "LFE", "MFE", 0.5, "b2p");
-
- % vector from HJC to 1/2 epi vec
-ind = ind +1;
-mb_c{ind,1} = "FE_Mechanical_axis";
-[mb_c{ind, 2}, mb_c{ind, 3}, mb_c{ind, 4}] = ...
-    findVector(mb_c, "MFE", "Epi_Midpoint", 1, "resultant");
-
-% find vector between mEd & muscle insertions in neutral
-for i = 1:1:muscle_count
-    % n_mEd = neutral hip postur_vector from middle Epicondyle distance
-    n_mEd_insertion{i,1} = mb_c{i,1}; % assign same row name
-    [n_mEd_insertion{i,2}, n_mEd_insertion{i,3}, n_mEd_insertion{i,4}] = ...
-        findVector(mb_c, mb_c{i,1}, "FE_Mechanical_axis", 1, "b2p");
-end
-
-% rotate femur into flexion by n-degrees until X degrees
-
- % create struct to hold all new muscle insertio data
- % index neutral coordinates for muscles
-for r = 1:1:muscle_count
-    insertion_s(1).in{r,1} = muscle_c{r,1};
-    for l = 5:1:7
-        insertion_s(1).in{r,l-3} = muscle_c{r,l};
-    end
-end
- % index neutral coordinates for bony THIGH landmarks
-for b = 5:1:7
-    for r = 1:1:4
-        landmark_s(1).in{b-4,r} = bone_c {b,r};
-    end
-end
+%% next steps
+ % rotate FMA for each desired hip flexion angle
+ % compute new insertion & femoral lm data (rotated FMA + n_EPI_MID_ins)
+ 
+%% everything below here needs to be refactored to match new approach
 
  % get neutral FME vector as regular array
 c_pos = size(mb_c,1);
@@ -101,25 +147,47 @@ for angle = 0:flexion_steps:max_flexion_angle
 end
 
 %% test plot3
-% keep building this out - not complete
+
+s_STOP = 10; % testing purposes
+s_START = 10;
+femoral_lm_count = size(landmark_s(1).in,1); % for landmark plotting
+FME_row_ind = 61; % '+ s' will be applied to compute correct FME coord
 fig1 = figure;
 view(168,2);
-step_number = 1; % testing purposes
-for s = 1:1:step_number
+for s = s_START:1:s_STOP
     hold on
     for plt = 1:1:muscle_count
         trend_style = getTrendStyle(plt, origin_ref);
         % z,x,y
-        plot3([origin_ref{plt,4}; insertion_s(1).in{plt,4}],...
-            [origin_ref{plt,2}; insertion_s(1).in{plt,2}],...
-            [origin_ref{plt,3}; insertion_s(1).in{plt,3}],...
+        plot3([origin_ref{plt,4}; insertion_s(s).in{plt,4}],...
+            [origin_ref{plt,2}; insertion_s(s).in{plt,2}],...
+            [origin_ref{plt,3}; insertion_s(s).in{plt,3}],...
             trend_style);
     end
-    plot3(0,0,0, 'ko'); % HJC
-    plot3([0; mb_c{61,4}], [0; mb_c{61,2}], [0; mb_c{61,3}], 'k-'); % fem mech axis
-    plot3(0,0,0, 'ko'); % Epicondyle centre
+     % FME
+    FME_row_ind = FME_row_ind + s;
+    plot3([0; mb_c{FME_row_ind,4}],...
+            [0; mb_c{FME_row_ind,2}],...
+            [0; mb_c{FME_row_ind,3}], 'k-');
+     % plot femoral boney landmarks -> these change with FME rotation
+    for b = 1:1:femoral_lm_count
+        plot3([landmark_s(s).in{b,4}],...
+                [landmark_s(s).in{b,2}],...
+                    [landmark_s(s).in{b,3}], 'k*');
+    end
+
+     % plot pelvis boney landmarks -> these DO NOT change with FME rotation
+    for pb = 1:1:4
+        pbplot = plot3(bone_c{pb,4},...
+            bone_c{pb,2},...
+            bone_c{pb,3}, '*');
+        pbplot.Color = '#A2142F';
+    end
+
     hold off
-    cf = getframe(fig1); % capture current plot as movie
+    %pause(3);
+    %close;
+    %cf = getframe(fig1); % capture current plot as movie
     %hold_frames{?} = frame2im(cf); %convert frame to RGB image
 end
 
